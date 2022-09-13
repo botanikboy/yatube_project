6b42dc02d4578a8a2330091b957eb8ce87ffdc73
Илья Савинкин <iasavinkin@gmail.com>
2022-09-13 20:10:02 +0300

    done homework

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,26 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
-#Main page
+# Main page
 def index(request):
-    return HttpResponse('оБложка')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    header_text = 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+        'header_text': header_text,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Бложики по по группам. Условие группировки: {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    header_text = 'Записи сообщества '
+    context = {
+        'group': group,
+        'posts': posts,
+        'header_text': header_text,
+    }
+
+    return render(request, 'posts/group_list.html', context)
